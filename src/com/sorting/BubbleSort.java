20724bb655d23e692af9671541dc85ecf38419e6
Vinay Kaushik <kumar.vinay64@gmail.com>
2020-05-06 08:28:58 +0530

    porblem solving

--- src/com/sorting/BubbleSort.java
+++ src/com/sorting/BubbleSort.java
@@ -7,7 +7,7 @@ public class BubbleSort {
 	
 	public static int[] buubleSort(int[] items) {
 		
-		for(int i=0;i<items.length-1;i++) {// pass loop 
+		for(int i=0;i<items.length;i++) {// pass loop 
 			for (int j = 0; j < items.length-1; j++) {
 				if (items[j]>items[j+1]) {
 					int temp=items[j];
@@ -25,7 +25,7 @@ public class BubbleSort {
 	public static void main(String[] args) {
 		
 		Random random= new Random();
-		int[] items= new int[10];// Random array of 10 items to be sorted
+		int[] items= new int[1000];// Random array of 10 items to be sorted
 		
 		for (int i = 0; i < items.length; i++) {
 			items[i]=random.nextInt(100);
